a67e0c60d1a5942c20b7e9f8888448902d5d3fde
세컨 <duwnsgur11012@gmail.com>
2023-07-22 18:38:10 +0900

    `get_cogs()`에 `Cogs.Core`가 맨 앞에 들어감

--- utility.py
+++ utility.py
@@ -15,10 +15,12 @@ def log(message: str):
     time = datetime.now(timezone('Asia/Seoul')).strftime("%y%m%d%H%M%S")
     print(f"[{time}] {message}")
 
-def get_cogs() -> map:
-    """ Cogs 폴더에 있는 모든 Cog 목록을 반환한다. """
-    return map(lambda cog: f"Cogs.{cog[:-3]}",
-        filter(lambda file: file.endswith(".py"), listdir("Cogs")))
+def get_cogs() -> list[str]:
+    """ Cogs 폴더에 있는 모든 Cog 목록을 반환한다. Cogs.Core가 제일 먼저 있다. """
+    cogs = list(map(lambda cog: f"Cogs.{cog[:-3]}",
+        filter(lambda file: file.endswith(".py"), listdir("Cogs"))))
+    cogs.sort(key=lambda cog: cog != "Cogs.Core")
+    return cogs
 
 def cog_logger(setup):
     """ Cog 로드 시 로그를 출력하는 데코레이터 """
